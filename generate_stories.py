e4f0fcb5f941fd806a28d41a0fa97dcfac5bd4e4
mat <matdallen@gmail.com>
2024-07-29 17:50:30 -0700

    openai stories

--- generate_stories.py
+++ generate_stories.py
@@ -31,48 +31,64 @@ def create_tiny_story_prompt(params: StoryParams):
     prompt = prompt_template.format(**locals())
     return prompt
 
+
+def generate_content(gen_model, prompt):
+    match gen_model:
+        case "openai":
+            client = OpenAI()
+            story = client.chat.completions.create(
+                model="gpt-4o",
+                messages=[
+                    {"role": "user", "content": prompt}
+                ]
+            )
+            return story.choices[0].message.content
     
-def generate_tiny_story(gen_model, params):
+def generate_tiny_story(gen_model, params: StoryParams):
     prompt = create_tiny_story_prompt(params)    
-    story = gen_model.generate_content(prompt)
+    story = generate_content(gen_model, prompt)
+    return story
     
-    try:
-        story_text = story.text
-    except ValueError as e:
-        print('Error:', e)
-        return None
-    json_struct = {'instruction': {
-        'features': list(params.story_features), 
-        'prompt': prompt,
-        'words': [params.verb, params.noun, params.adjective]
-        },
-        'story': story_text,
-        'source': 'gemini-1.5-flash'}
-    return json_struct
+    #try:
+        #story_text = story.text
+    #except ValueError as e:
+        #print('Error:', e)
+        #return None
+    #json_struct = {'instruction': {
+        #'features': list(params.story_features), 
+        #'prompt': prompt,
+        #'words': [params.verb, params.noun, params.adjective]
+        #},
+        #'story': story_text,
+        #'source': 'gemini-1.5-flash'}
+    #return json_struct
 
 
-def generate_and_log_tiny_stories(gen_model, num_stories: int):#, thread_id: int = 0):    
+def generate_and_log_tiny_stories(gen_model):#, thread_id: int = 0):    
     rand_verb = random.choice(test_verbs)
     rand_noun = random.choice(test_nouns)
     rand_adj  = random.choice(test_adjectives)
     rand_feat = random.choices(features)
-    params = StoryParams(rand_verb, rand_noun, rand_adj, rand_feat)
 
-    for i in range(0, num_stories):
-        try:
-            story = generate_tiny_story(gen_model, params)
-            if story:
-                md5 = hashlib.md5(story['story'].encode()).hexdigest()
-                with open(f'spanish_stories/generated_story_{i}_{md5}.json', 'w') as fp:
-                    json.dump(story, fp)
-            else:
-                print('Error generating story', i)
-        except Exception as e:
-            print('Error generating story', i, e)
-            print('sleeping for 10 seconds at index', i)
-            time.sleep(10)
-        if i % 1000 == 0:
-            print('At index', i)
+    params = StoryParams(rand_verb, rand_noun, rand_adj, rand_feat)
+    story = generate_tiny_story(gen_model, params)
+    return story
+
+    #for i in range(0, num_stories):
+        #try:
+            #story = generate_tiny_story(gen_model, params)
+            #if story:
+                #md5 = hashlib.md5(story['story'].encode()).hexdigest()
+                #with open(f'spanish_stories/generated_story_{i}_{md5}.json', 'w') as fp:
+                    #json.dump(story, fp)
+            #else:
+                #print('Error generating story', i)
+        #except Exception as e:
+            #print('Error generating story', i, e)
+            #print('sleeping for 10 seconds at index', i)
+            #time.sleep(10)
+        #if i % 1000 == 0:
+            #print('At index', i)
 
 
 def main():
@@ -89,6 +105,7 @@ def main():
     rand_feat = random.choices(features)
     params = StoryParams(rand_verb, rand_noun, rand_adj, rand_feat)
     print(create_tiny_story_prompt(params))
+    print(generate_and_log_tiny_stories("openai"))
 
     
 if __name__ == '__main__':
